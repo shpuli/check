a28d41265cde8e0124c5bd3cbf3d0a1e3d4d7aed
shpuli <shp.julia@gmail.com>
2016-11-28 18:56:00 +0300

    add files

---
+++ main.js
@@ -0,0 +1,29 @@
+requirejs.config({
+  //By default load any module IDs from js/lib
+  baseUrl: 'node_modules/roundme-ios-gyro-patch/',
+  //except, if the module ID starts with "app",
+  //load it from the js/app directory. paths
+  //config is relative to the baseUrl, and
+  //never includes a ".js" extension since
+  //the paths config could be for a directory.
+  paths: {
+    app: './main'
+  }
+});
+
+// Start the main app logic.
+// requirejs(['jquery', 'canvas', 'app/sub'],
+//   function   ($,        canvas,   sub) {
+//     //jQuery, canvas and the app/sub module are all
+//     //loaded and can be used here now.
+//   });
+
+var myHeading = document.querySelector('h1');
+
+
+
+define(['roundme-ios-gyro-patch'], function (gyro) {
+  console.log(gyro)
+});
+
+myHeading.textContent = 'piu-piu';
